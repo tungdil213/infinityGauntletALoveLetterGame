38b9448020867c601d47d9254b1e200cf6b1e4fb
Tungdil213 <admin@herric.de>
2023-06-13 22:28:31 +0200

    add guards with the tests

--- src/machine/guards.ts
+++ src/machine/guards.ts
@@ -8,11 +8,7 @@ export const canJoinGuard: GameGuard<"join"> = (context, event) => {
 };
 
 export const canChooseSideGuard: GameGuard<"chooseSide"> = (context, event) => {
-  return (
-    [Side.HEROES, Side.THANOS].includes(event.side) &&
-    context.players.find((p) => p.id === event.playerId) !== undefined &&
-    context.players.find((p) => p.side === event.side) === undefined
-  );
+  return context.players.find((p) => p.id === event.playerId) !== undefined;
 };
 
 export const canLeaveGuard: GameGuard<"leave"> = (context, event) => {
@@ -20,7 +16,14 @@ export const canLeaveGuard: GameGuard<"leave"> = (context, event) => {
 };
 
 export const canStartGameGuard: GameGuard<"start"> = (context, event) => {
-  return true; // TODO
+  return (
+    1 < context.players.length &&
+    context.players.length < 6 &&
+    context.players.find((p) => p.id === event.playerId) !== undefined &&
+    context.players.every((p) => "side" in p) &&
+    context.players.find((p) => p.side === Side.HEROES) !== undefined &&
+    context.players.find((p) => p.side === Side.THANOS) !== undefined
+  );
 };
 export const isWiningMoveGuard: GameGuard<"chooseAbility"> = (
   context,
---
+++ test/guards/guards.test.ts
@@ -0,0 +1,292 @@
+import { ThanosAbilities } from "./../../src/types";
+import { InterpreterFrom } from "xstate";
+import {
+  canJoinGuard,
+  canChooseSideGuard,
+  canStartGameGuard,
+} from "../../src/machine/guards";
+import { describe, expect, it } from "vitest";
+import { GameMachine, makeGame } from "../../src/machine/GameMachine";
+import { GameStates, Side } from "../../src/types";
+
+describe("machine/Guards", () => {
+  let machine: InterpreterFrom<typeof GameMachine>;
+
+  describe("canJoinGuard", () => {
+    it("should return true if the player can join", () => {
+      machine = makeGame(GameStates.PLAY, {
+        players: [
+          {
+            id: "1",
+            name: "1",
+          },
+          {
+            id: "2",
+            name: "2",
+          },
+        ],
+      });
+      const event = {
+        playerId: "3",
+        name: "3",
+        type: "join" as const,
+      };
+      expect(canJoinGuard(machine.getSnapshot().context, event)).toBe(true);
+    });
+
+    it("should return false if the player is already in the game", () => {
+      machine = makeGame(GameStates.PLAY, {
+        players: [
+          {
+            id: "1",
+            name: "1",
+          },
+          {
+            id: "2",
+            name: "2",
+          },
+          {
+            id: "3",
+            name: "3",
+          },
+        ],
+      });
+      const event = {
+        playerId: "3",
+        name: "3",
+        type: "join" as const,
+      };
+      expect(canJoinGuard(machine.getSnapshot().context, event)).toBe(false);
+    });
+
+    it("should return false if the game is full", () => {
+      machine = makeGame(GameStates.PLAY, {
+        players: [
+          {
+            id: "1",
+            name: "1",
+          },
+          {
+            id: "2",
+            name: "2",
+          },
+          {
+            id: "3",
+            name: "3",
+          },
+          {
+            id: "4",
+            name: "4",
+          },
+          {
+            id: "5",
+            name: "5",
+          },
+          {
+            id: "6",
+            name: "6",
+          },
+        ],
+      });
+      const event = {
+        playerId: "7",
+        name: "7",
+        type: "join" as const,
+      };
+      expect(canJoinGuard(machine.getSnapshot().context, event)).toBe(false);
+    });
+  });
+
+  describe("canChooseSideGuard", () => {
+    it("should return true if the player can choose a side", () => {
+      machine = makeGame(GameStates.PLAY, {
+        players: [
+          {
+            id: "1",
+            name: "1",
+          },
+          {
+            id: "2",
+            name: "2",
+          },
+        ],
+      });
+      const event = {
+        playerId: "2",
+        side: Side.HEROES,
+        type: "chooseSide" as const,
+      };
+      expect(canChooseSideGuard(machine.getSnapshot().context, event)).toBe(
+        true
+      );
+    });
+
+    it("must send back true if the player can choose a side several times", () => {
+      machine = makeGame(GameStates.PLAY, {
+        players: [
+          {
+            id: "1",
+            name: "1",
+          },
+          {
+            id: "2",
+            name: "2",
+          },
+        ],
+      });
+      const eventHeros = {
+        playerId: "2",
+        side: Side.HEROES,
+        type: "chooseSide" as const,
+      };
+      expect(
+        canChooseSideGuard(machine.getSnapshot().context, eventHeros)
+      ).toBe(true);
+      const eventThanos = {
+        playerId: "2",
+        side: Side.THANOS,
+        type: "chooseSide" as const,
+      };
+      expect(
+        canChooseSideGuard(machine.getSnapshot().context, eventThanos)
+      ).toBe(true);
+      expect(
+        canChooseSideGuard(machine.getSnapshot().context, eventHeros)
+      ).toBe(true);
+    });
+
+    it("should return false if the player is not in the game", () => {
+      machine = makeGame(GameStates.PLAY, {
+        players: [
+          {
+            id: "1",
+            name: "1",
+          },
+          {
+            id: "2",
+            name: "2",
+          },
+        ],
+      });
+      const event = {
+        playerId: "3",
+        side: Side.HEROES,
+        type: "chooseSide" as const,
+      };
+      expect(canChooseSideGuard(machine.getSnapshot().context, event)).toBe(
+        false
+      );
+    });
+  });
+
+  describe("canStartGameGuard", () => {
+    it("should return true if the game can be started", () => {
+      machine = makeGame(GameStates.PLAY, {
+        players: [
+          {
+            id: "1",
+            name: "1",
+            side: Side.THANOS,
+          },
+          {
+            id: "2",
+            name: "2",
+            side: Side.HEROES,
+          },
+          {
+            id: "3",
+            name: "3",
+            side: Side.HEROES,
+          },
+        ],
+      });
+      const event = { playerId: "1", type: "start" as const };
+      expect(canStartGameGuard(machine.getSnapshot().context, event)).toBe(
+        true
+      );
+    });
+
+    it("should return false if there are not enough players", () => {
+      machine = makeGame(GameStates.PLAY, {
+        players: [{ id: "1", name: "1", side: Side.HEROES }],
+      });
+      const event = { playerId: "1", type: "start" as const };
+      expect(canStartGameGuard(machine.getSnapshot().context, event)).toBe(
+        false
+      );
+    });
+
+    it("should return false if there are too many players", () => {
+      machine = makeGame(GameStates.PLAY, {
+        players: [
+          { id: "1", name: "1", side: Side.HEROES },
+          { id: "2", name: "2", side: Side.HEROES },
+          { id: "3", name: "3", side: Side.THANOS },
+          { id: "4", name: "4", side: Side.THANOS },
+          { id: "5", name: "5", side: Side.THANOS },
+          { id: "6", name: "6", side: Side.THANOS },
+        ],
+      });
+      const event = { playerId: "1", type: "start" as const };
+      expect(canStartGameGuard(machine.getSnapshot().context, event)).toBe(
+        false
+      );
+    });
+
+    it("should return false if the player is not in the game", () => {
+      machine = makeGame(GameStates.PLAY, {
+        players: [
+          { id: "1", name: "1", side: Side.HEROES },
+          { id: "2", name: "2", side: Side.HEROES },
+          { id: "3", name: "3", side: Side.THANOS },
+        ],
+      });
+      const event = { playerId: "4", type: "start" as const };
+      expect(canStartGameGuard(machine.getSnapshot().context, event)).toBe(
+        false
+      );
+    });
+
+    it("should return false if not all players have chosen a side", () => {
+      machine = makeGame(GameStates.PLAY, {
+        players: [
+          { id: "1", name: "1", side: Side.HEROES },
+          { id: "2", name: "2", side: Side.THANOS },
+          { id: "3", name: "3" },
+        ],
+      });
+      const event = { playerId: "1", type: "start" as const };
+      expect(canStartGameGuard(machine.getSnapshot().context, event)).toBe(
+        false
+      );
+    });
+
+    it("should return false if there are no heroes", () => {
+      machine = makeGame(GameStates.PLAY, {
+        players: [
+          { id: "1", name: "1", side: Side.THANOS },
+          { id: "2", name: "2", side: Side.THANOS },
+          { id: "3", name: "3", side: Side.THANOS },
+        ],
+      });
+      const event = { playerId: "1", type: "start" as const };
+      expect(canStartGameGuard(machine.getSnapshot().context, event)).toBe(
+        false
+      );
+    });
+
+    it("should return false if there are no thanos", () => {
+      machine = makeGame(GameStates.PLAY, {
+        players: [
+          { id: "1", name: "1", side: Side.HEROES },
+          { id: "2", name: "2", side: Side.HEROES },
+          { id: "3", name: "3", side: Side.HEROES },
+        ],
+      });
+      const event = { playerId: "1", type: "start" as const };
+      expect(canStartGameGuard(machine.getSnapshot().context, event)).toBe(
+        false
+      );
+    });
+  });
+});
