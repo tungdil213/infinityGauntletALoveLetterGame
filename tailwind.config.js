5cf269ec19bf45fbc5d67fccdd0a865e9bec9ffe
Tungdil213 <admin@herric.de>
2024-07-26 22:05:04 +0200

    chore: Update Tailwind CSS configuration and dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./src/presentation/**/*.edge",
+      "./src/presentation/**/*.{js,ts,jsx,tsx,vue}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
