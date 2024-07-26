37857047009becfbbdfe4d20897cbe98984c13b2
Tungdil213 <admin@herric.de>
2024-07-26 15:53:08 +0200

    chore: Update lobby list page with user and lobby data

--- src/presentation/inertia/pages/lobby/list.tsx
+++ src/presentation/inertia/pages/lobby/list.tsx
@@ -1,8 +1,13 @@
 import ListLobbiesController from '#app/http/lobby/controllers/list_lobbies_controller'
+import { LobbyInterface } from '#domain/gameHub/lobby/entities/lobby_interface'
 import type { InferPageProps } from '@adonisjs/inertia/types'
-import { Head } from '@inertiajs/react'
+import { Head, Link, router } from '@inertiajs/react'
 
 export default function Home(props: Readonly<InferPageProps<ListLobbiesController, 'handle'>>) {
+  function onClickCreateLobby(e: React.FormEvent<EventTarget>) {
+    e.preventDefault()
+    router.post('/lobby/create')
+  }
   return (
     <>
       <Head title="List Lobby" />
@@ -10,13 +15,52 @@ export default function Home(props: Readonly<InferPageProps<ListLobbiesControlle
         <h1>List Lobby</h1>
         <p>Here is the lobbies</p>
         <p>
-          <b>lobby</b>
-          {JSON.stringify(props.lobbies)}
-        </p>
-        <p>
           <b>user</b>
-          {JSON.stringify(props.user)}
+          <div>
+            <p>
+              <b>id</b>
+              {props.user.id}
+            </p>
+            <p>
+              <b>username</b>
+              {props.user.username}
+            </p>
+            <p>
+              <b>email</b>
+              {props.user.email}
+            </p>
+            <p>
+              <b>created_at</b>
+              {props.user.created_at}
+            </p>
+            <p>
+              <b>updated_at</b>
+              {props.user.updated_at}
+            </p>
+          </div>
         </p>
+        <b>lobby</b>
+        {props.lobbies.map((lobby: LobbyInterface) => (
+          <div key={lobby.id}>
+            <Link href={`/lobby/${lobby.id}`}>
+              <b>id</b>
+              {lobby.id}
+            </Link>
+            <p>
+              <b>status</b>
+              {lobby.status}
+            </p>
+            <p>
+              <b>created_at</b>
+              {lobby.createdAt?.toDateString?.()}
+            </p>
+            <p>
+              <b>updated_at</b>
+              {lobby.updatedAt?.toDateString?.()}
+            </p>
+          </div>
+        ))}
+        <button onClick={onClickCreateLobby}>Create Lobby</button>
       </div>
     </>
   )
--- src/presentation/inertia/pages/lobby/show.tsx
+++ src/presentation/inertia/pages/lobby/show.tsx
@@ -1,12 +1,13 @@
 import ShowLobbyController from '#app/http/lobby/controllers/show_lobby_controller'
 import type { InferPageProps } from '@adonisjs/inertia/types'
-import { Head } from '@inertiajs/react'
+import { Head, Link } from '@inertiajs/react'
 
 export default function Home(props: Readonly<InferPageProps<ShowLobbyController, 'handle'>>) {
   return (
     <>
       <Head title="Show Lobby" />
       <div className="container">
+        <Link href="/lobby/">List Lobbies</Link>
         <h1>Show Lobby</h1>
         <p>Here is your lobby</p>
         <p>
